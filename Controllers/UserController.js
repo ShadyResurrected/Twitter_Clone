8395f1432eb2b8194f872c98289668b4c9acdd45
ShadyResurrected <cchandra554@gmail.com>
2022-12-26 11:11:09 +0530

    Fetch all users as people you may know

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,5 +1,23 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
+
+
+// get all users
+export const getAllUsers = async (req,res) => {
+  try {
+    let users = await UserModel.find()
+    users = users.map((user) => {
+      const {password, ...otherDetails} = user._doc
+      return otherDetails
+    })
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+
 // get a User
 export const getUser = async (req, res) => {
   const id = req.params.id;
@@ -22,9 +40,9 @@ export const getUser = async (req, res) => {
 // update a user
 export const updateUser = async (req, res) => {
   const id = req.params.id;
-  const { currentUserId, currentUserAdminStatus, password } = req.body;
+  const { _id, currentUserAdminStatus, password } = req.body;
 
-  if (id === currentUserId || currentUserAdminStatus) {
+  if (id === _id) {
     try {
       if (password) {
         const salt = await bcrypt.genSalt(10);
@@ -35,7 +53,12 @@ export const updateUser = async (req, res) => {
         new: true,
       });
 
-      res.status(200).json(user);
+      const token = jwt.sign(
+        { username: user.username, id: user._id },
+        process.env.JWT_KEY,
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({user,token});
     } catch (error) {
       res.status(500).json(error);
     }
