6e41ce2f1f9b975ce3731398a84ecd0ce84b64fd
ShadyResurrected <cchandra554@gmail.com>
2022-12-26 14:02:39 +0530

    Added following functionality

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const User = ({person}) => {
-    const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER
-    const handleFollow = () => {
-        
-    }
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unFollowUser } from "../../redux/actions/userAction";
+
+const User = ({ person }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.AuthReducer.authData);
+  const [following, setFollowing] = useState(
+    person.followers.includes(user._id)
+  );
+
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const handleFollow = () => {
+    following
+      ? dispatch(unFollowUser(person._id, user))
+      : dispatch(followUser(person._id, user));
+
+      setFollowing((prev) => !prev)
+  };
   return (
     <div className="follower">
       <div>
-        <img src={person.profilePicutre ? serverPublic + person.profilePicutre : serverPublic + "DefImages/def_profile_pic.png"} alt="" className="followerImage" />
+        <img
+          src={
+            person.profilePicutre
+              ? serverPublic + person.profilePicutre
+              : serverPublic + "DefImages/def_profile_pic.png"
+          }
+          alt=""
+          className="followerImage"
+        />
         <div className="name">
           <span>{person.firstname}</span>
           <span>{person.username}</span>
         </div>
       </div>
-      <button className="button fc-button" onClick={handleFollow}>Follow</button>
+      <button className={following ? "button fc-button UnfollowButton" : "button fc-button"} onClick={handleFollow}>
+        {following ? "Unfollow" : "Follow"}
+      </button>
     </div>
   );
 };
